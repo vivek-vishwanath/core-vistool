5a42f2bb6b6e740116ee36a0ed1ab2ae403598e7
Vivek Vishwanath <vivs005@gmail.com>
2025-05-08 20:02:52 -0400

    Setup initial editor & display

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,34 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  const pixels = Array.from({ length: 32 }, () =>
+          Array.from({ length: 32 }, () => Math.random() < 0.3)
+  );
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div style="display: flex; padding: 1rem; gap: 1rem; background: #f3f4f6;">
+    <div>
+      <div style="display: flex; flex-direction: row; align-items: center; gap: 1rem;">
+        <!-- Code Editor -->
+        <div style="background: white; border-radius: 0.5rem; box-shadow: 0 1px 3px rgba(0,0,0,0.1); width: 40vw">
+          <div class="editor-tabs">
+            <div class="tab active-tab">main.s</div>
+            <div class="tab">handler.s</div>
+          </div>
+          <div style="height: 10rem; background: #f3f4f6; padding: 0.5rem; font-family: monospace; font-size: 0.875rem;">
+            ; Assembly code goes here...
+          </div>
+        </div>
+        <!-- Pixel Display -->
+        <div class="pixel-display">
+          {#each pixels as row, i}
+            {#each row as pixel, j}
+            <div style="display: flex; flex-direction: row; align-items: center; gap: 1rem;">
+              <div class='pixel' class:green={pixel}></div>
+            </div>
+            {/each}
+          {/each}
+        </div>
+      </div>
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
